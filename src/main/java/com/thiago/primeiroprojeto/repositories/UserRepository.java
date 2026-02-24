c4793bc1e35044bb322cca8c298c08d8c8c62d8a
Thiago Santos <thiagosm2006@gmail.com>
2026-02-24 16:01:47 -0300

    feat:JPA repository, dependence injection, database seeding

---
+++ src/main/java/com/thiago/primeiroprojeto/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.thiago.primeiroprojeto.repositories;
+
+import com.thiago.primeiroprojeto.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
