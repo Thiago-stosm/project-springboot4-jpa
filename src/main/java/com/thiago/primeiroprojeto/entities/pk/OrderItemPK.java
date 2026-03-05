b13ae43d4e08f5a4357a515fc7c059e74a4d2c9e
Thiago Santos <thiagosm2006@gmail.com>
2026-03-05 14:38:18 -0300

    feat: OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/thiago/primeiroprojeto/entities/pk/OrderItemPK.java
@@ -0,0 +1,52 @@
+package com.thiago.primeiroprojeto.entities.pk;
+
+import com.thiago.primeiroprojeto.entities.Order;
+import com.thiago.primeiroprojeto.entities.Product;
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name="order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name="product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
